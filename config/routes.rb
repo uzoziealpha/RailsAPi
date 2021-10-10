a1ef84da9a736b10ba57d4e2b5ea54889b16e450
Uzoziealpha <uzozie.obi@icloud.com>
2021-10-10 10:30:37 +0800

    Apicommit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+      
+     #namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
